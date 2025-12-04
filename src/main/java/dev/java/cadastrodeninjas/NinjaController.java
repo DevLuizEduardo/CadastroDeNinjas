d9f0f1feb78c79d5faf8677abc8d213cee19e3a5
Admin <devluizeduardo@gmail.com>
2025-12-04 20:46:45 -0300

    Criação da Model

--- src/main/java/dev/java/cadastrodeninjas/NinjaController.java
+++ src/main/java/dev/java/cadastrodeninjas/NinjaController.java
@@ -1,4 +1,16 @@
 package dev.java.cadastrodeninjas;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class NinjaController {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+
+        return "Boas vindas";
+    }
 }
--- src/main/java/dev/java/cadastrodeninjas/NinjaModel.java
+++ src/main/java/dev/java/cadastrodeninjas/NinjaModel.java
@@ -1,4 +1,30 @@
 package dev.java.cadastrodeninjas;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro")
 public class NinjaModel {
+
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String nome;
+    String email;
+    int idade;
+
+    public NinjaModel() {
+
+    }
+
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+
+
 }
