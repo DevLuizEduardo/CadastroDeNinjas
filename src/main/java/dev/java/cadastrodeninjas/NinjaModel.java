d2dcb0a5c00caf56387fb6c673d24ff4095c3680
Luiz Eduardo <103207346+DevLuizEduardo@users.noreply.github.com>
2025-12-04 20:55:27 -0300

    New README

--- src/main/java/dev/java/cadastrodeninjas/NinjaModel.java
+++ src/main/java/dev/java/cadastrodeninjas/NinjaModel.java
@@ -10,10 +10,10 @@ public class NinjaModel {
 
 @Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
-    String nome;
-    String email;
-    int idade;
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
 
     public NinjaModel() {
 
@@ -26,5 +26,35 @@ public class NinjaModel {
     }
 
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
 }
