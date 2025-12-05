0df8373e4237c1c90a31b678d250e274d01aa446
Admin <devluizeduardo@gmail.com>
2025-12-05 18:35:14 -0300

    feat:Coloca o Lombok na classe para abstracao

--- src/main/java/dev/java/cadastrodeninjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java/cadastrodeninjas/Missoes/MissoesModel.java
@@ -2,11 +2,17 @@ package dev.java.cadastrodeninjas.Missoes;
 
 import dev.java.cadastrodeninjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissoesModel {
 
     @Id
--- src/main/java/dev/java/cadastrodeninjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java/cadastrodeninjas/Ninjas/NinjaModel.java
@@ -3,11 +3,17 @@ package dev.java.cadastrodeninjas.Ninjas;
 
 import dev.java.cadastrodeninjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
 
 
@@ -23,46 +29,5 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id")//Foreing Key ou Chave Estrangeira
     private MissoesModel missoes;
 
-    public NinjaModel() {
 
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
