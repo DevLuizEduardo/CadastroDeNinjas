7b5a19ee0c9486868d2c22e2bac492f78e3f0ac2
Luiz Eduardo <devluizeduardo@gmail.com>
2025-12-08 17:38:01 -0300

    fix:Inclui a anotation @JsonIgnore para prevenir o erro de serializacao

--- src/main/java/dev/java/cadastrodeninjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java/cadastrodeninjas/Missoes/MissoesModel.java
@@ -1,5 +1,6 @@
 package dev.java.cadastrodeninjas.Missoes;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import dev.java.cadastrodeninjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -25,5 +26,6 @@ public class MissoesModel {
 
     //Uma missão para muitos Ninjas
     @OneToMany(mappedBy = "missoes")
+    @JsonIgnore
     private List<NinjaModel> ninjas;
 }
