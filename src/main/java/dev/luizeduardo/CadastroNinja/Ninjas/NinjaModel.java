d0c02bd762a3bbe20fd5b3ed261e6388445a1391
Luiz Eduardo <devluizeduardo@gmail.com>
2026-05-19 12:25:59 -0300

    feat: Adiciona o lombok nas classes para abstracao

--- src/main/java/dev/luizeduardo/CadastroNinja/Ninjas/NinjaModel.java
+++ src/main/java/dev/luizeduardo/CadastroNinja/Ninjas/NinjaModel.java
@@ -2,12 +2,18 @@ package dev.luizeduardo.CadastroNinja.Ninjas;
 
 import dev.luizeduardo.CadastroNinja.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 //Entity Ele transforma uma classe em uma entidade do DB
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,36 +26,4 @@ public class NinjaModel {
     @JoinColumn(name = "misssoes_id")
     private MissoesModel missoes;
 
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
