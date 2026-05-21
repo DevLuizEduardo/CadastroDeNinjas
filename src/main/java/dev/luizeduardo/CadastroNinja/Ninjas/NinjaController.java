acf59383532758f3ad4650692109345b9c3b6258
Luiz Eduardo <devluizeduardo@gmail.com>
2026-05-21 14:28:39 -0300

    fix:Alterar o nome das rotas do controller ninjas

--- src/main/java/dev/luizeduardo/CadastroNinja/Ninjas/NinjaController.java
+++ src/main/java/dev/luizeduardo/CadastroNinja/Ninjas/NinjaController.java
@@ -3,7 +3,7 @@ package dev.luizeduardo.CadastroNinja.Ninjas;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("ninjas")
 public class NinjaController {
 
     @GetMapping("/boasvindas")
@@ -17,13 +17,13 @@ public class NinjaController {
         return "Ninja Criado";
     }
     //Mostrar todos os Ninjas(READ)
-    @GetMapping("/todos")
+    @GetMapping("/listar")
     public String mostrarTodosOsNinjas(){
         return "Mostrar Ninjas";
     }
 
     // Mostrar Ninja por ID (READ)
-    @GetMapping("/todosID")
+    @GetMapping("/listarID")
     public String mostrarTodosOsNinjasPorId(){
         return "Mostrar Ninja por ID";
     }
