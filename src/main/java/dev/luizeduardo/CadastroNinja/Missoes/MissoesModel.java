4ca6a1ca18f12ceb563294fc5a14ab7dc48b27fd
Luiz Eduardo <devluizeduardo@gmail.com>
2026-05-21 16:15:59 -0300

    fix:Inclui a notation @JsonIgnore no meu MissoesModel para previnir o erro de serializacao

--- src/main/java/dev/luizeduardo/CadastroNinja/Missoes/MissoesModel.java
+++ src/main/java/dev/luizeduardo/CadastroNinja/Missoes/MissoesModel.java
@@ -1,5 +1,6 @@
 package dev.luizeduardo.CadastroNinja.Missoes;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import dev.luizeduardo.CadastroNinja.Ninjas.NinjaModel;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -23,5 +24,6 @@ public class MissoesModel {
     private String dificuldade;
 
     @OneToMany(mappedBy = "missoes")
+    @JsonIgnore
     private List<NinjaModel> ninjas;
 }
