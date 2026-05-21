4d80e2d47c4fa0b61b9b8437a519ba09fab1f6df
Luiz Eduardo <devluizeduardo@gmail.com>
2026-05-21 17:07:23 -0300

    feat: Adiciona roda de findById

--- src/main/java/dev/luizeduardo/CadastroNinja/Ninjas/NinjaController.java
+++ src/main/java/dev/luizeduardo/CadastroNinja/Ninjas/NinjaController.java
@@ -31,9 +31,10 @@ public class NinjaController {
     }
 
     // Mostrar Ninja por ID (READ)
-    @GetMapping("/listarID")
-    public String mostrarTodosOsNinjasPorId(){
-        return "Mostrar Ninja por ID";
+    @GetMapping("/listar/{id}")
+    public NinjaModel listarNinjasPorId(@PathVariable Long id){
+
+        return ninjaService.listarNinjasPorId(id);
     }
 
     //Alterar dados dos Ninjas(UPDATE)
--- src/main/java/dev/luizeduardo/CadastroNinja/Ninjas/NinjaService.java
+++ src/main/java/dev/luizeduardo/CadastroNinja/Ninjas/NinjaService.java
@@ -3,6 +3,7 @@ package dev.luizeduardo.CadastroNinja.Ninjas;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class NinjaService {
@@ -19,6 +20,15 @@ public class NinjaService {
     public List<NinjaModel> listarNinjas(){
 
         return ninjaRepository.findAll();
+
+    }
+
+    //Listar Ninjas por ID
+
+    public NinjaModel listarNinjasPorId(Long id){
+
+        Optional<NinjaModel>ninjaPorId = ninjaRepository.findById(id);
+        return ninjaPorId.orElse(null);
 
     }
 }
